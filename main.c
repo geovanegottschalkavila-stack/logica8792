0c8027fe58bf38a9d667e16c60ecdc5659fa1e74
geovanegottschalkavila-stack <geovanegottschalkavila@gmail.com>
2026-09-09 20:43:31 -0300

    maior que

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+    int main(){
+
+        int a = 5;
+        int b = 4;
+
+        printf("o maior número é: %d\n", (a + b));
+        printf("o resultado da subtração é: %d\n", (a - b));
+        printf("a multiplicação é: %d\n", (a * b));
+        printf("a divisão é: %d\n", (a / b));
+
+        return 0;
+    }
