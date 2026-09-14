a2c2c1cb2944d774df3b692c223fbe00b49bff7d
geovanegottschalkavila-stack <geovanegottschalkavila@gmail.com>
2026-09-14 19:35:21 -0300

    par e impar

--- main.c
+++ main.c
@@ -17,7 +17,11 @@
         printf("Digite um número: ");
         scanf("%d", &n);
 
-        printf("%s", n > 0 ? "positivo" : (n < 0 ? "negativo" : "zero"));
+        if(n % 2 == 0){
+            printf("\nPar");
+        }else{
+            printf("\nImpar");
+        }
 
         return 0;
     }
