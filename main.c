bfbd6eeeda51333470800363b4ef8aa1298dd533
geovanegottschalkavila-stack <geovanegottschalkavila@gmail.com>
2026-09-14 21:35:18 -0300

    menor e maior  numero a,bc,d,e

--- main.c
+++ main.c
@@ -12,7 +12,7 @@
        SetConsoleCP(65001);
        SetConsoleOutputCP(65001);
 
-        int a, b, c, d, e, menor;
+        int a, b, c, d, e, menor, maior;
 
         printf("digite um numero para a: ");
         scanf("%d", &a);
@@ -30,13 +30,20 @@
         scanf("%d", &d);
 
         menor = a;
+        maior = a;
 
         if(b < menor) menor = b;
         if(c < menor) menor = c;
         if(d < menor) menor = d;
         if(e < menor) menor = e;
-        
-        printf("menor: %d", menor);
+       
+        if(b > maior) maior = b;
+        if(c > maior) maior = c;
+        if(d > maior) maior = d;
+        if(e > maior) maior = e;
+
+        printf("menor: %d\n", menor);
+        printf("maior: %d", maior );
 
         return 0;
     }
