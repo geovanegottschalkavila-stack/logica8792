4be695030d8dac13880cce18073472fb7b83edf1
geovanegottschalkavila-stack <geovanegottschalkavila@gmail.com>
2026-09-16 21:44:02 -0300

    lados do triangulo

--- main.c
+++ main.c
@@ -7,29 +7,18 @@
        SetConsoleCP(65001);
        SetConsoleOutputCP(65001);
 
-       float n1, n2, n3, media;
+       float a, b, c;
 
-       printf("Digite a nota 1: ");
-       scanf("%d", &n1);
+       printf("digite os tres lados do triangulo");
+       scanf("%f %f %f", &a, &b, &c);
 
-        printf("digite a nota 1: ");
-        scanf("%f", &n1);
-        
-        printf("digite a nota 2: ");
-        scanf("%f", &n2);
-    
-        printf("digite a nota 3: ");
-        scanf("%f", &n3);
-
-        media = (n1 + n2 + n3) / 3;
-
-        if (media <= 4){
-            pritnf("aprovado");
-        }else if(media >= 7){
-            printf("recuperação");
-        }else{
-            printf("reprovado");
-        }
+       if(a == b && b == c){
+            printf("Equilátero!\n");
+       }else if(a == b || a == c || b == c){
+            printf("isoceles!\n");
+       }else{
+            printf("Escaleno!\n");
+       }
 
         return 0;
     }
