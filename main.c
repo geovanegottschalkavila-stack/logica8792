16f5a915143b7d554614279c293750a93c9b4583
geovanegottschalkavila-stack <geovanegottschalkavila@gmail.com>
2026-09-18 21:08:02 -0300

    multiplos

--- main.c
+++ main.c
@@ -7,16 +7,19 @@
        SetConsoleCP(65001);
        SetConsoleOutputCP(65001);
 
-   char letra;
-    printf("digite uma letra: ");
-    scanf("%c", &letra);
+       int n;
+       printf("Digite um número: ");
+       scanf("%d", &n);
 
-    if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u' ||
-        letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U'){
-            printf("vogal\n");
-        }else{
-            printf("consoante\n");
-        }
+       if(n % 3 == 0 && n % 5 == 0){
+            printf("Múltiplo de 3 e 5\n");
+       }else if("n % 3 == 0"){
+            printf("Múltiplo de 3\n");
+       }else if(n % 5 == 0){
+            printf("Múltiplo de 5\n");
+       }else{
+            printf("Não é múltiplo de 3 nem de 5\n");
+       }
         return 0;
 
     }
