04b79cc43fb27b17e9418dd53b59cf5ecbad4054
geovanegottschalkavila-stack <geovanegottschalkavila@gmail.com>
2026-09-21 19:07:09 -0300

    for bommm

--- main.c
+++ main.c
@@ -7,28 +7,11 @@
        SetConsoleCP(65001);
        SetConsoleOutputCP(65001);
 
-       float peso, altura, imc;
-
-       printf("Digite o peso (kg): ");
-       scanf("%F", &altura);
-
-       printf("Digite a altura (m): ");
-       scanf("%f", &altura);
-
-       imc = peso / (altura * altura);
-
-       if(imc < 18.5){
-            printf("seu peso é: %f\n", peso);
-            printf("sua altura é: %f\n", altura);
-            printf("Classificaçõa: Abaixo do peso!\n");
-       }else if(imc < 25){
-            printf("Classificação: Peso normal!\n");
-       }else if(imc < 30){
-            printf("Classificação: sobrepeso!\n");
-       }else{
-            printf("classificação: Obesidade!\n");
+       for(int i = 10; i >= 0; i--){
+            printf("%d\n", i);
        }
 
+       printf("\nBooommmmmmmm");
 
         return 0;
 
