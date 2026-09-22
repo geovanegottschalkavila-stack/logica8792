47bca4d6287d07d31a2c044c0acfc84903a03a64
geovanegottschalkavila-stack <geovanegottschalkavila@gmail.com>
2026-09-21 21:49:44 -0300

    declarar os arreios

--- main.c
+++ main.c
@@ -7,11 +7,16 @@
        SetConsoleCP(65001);
        SetConsoleOutputCP(65001);
         
-       int numeros[3] = {1, 2, 3};
+       int numeros[6];
 
        numeros[0] = 10;
+       numeros[1] = 20;
+       numeros[2] = 30;
+       numeros[3] = 40;
+       numeros[4] = 50;
+       numeros[5] = 60;
 
-       printf("%d", numeros[0]);
+       printf("%d", numeros[5]);
 
 
 
