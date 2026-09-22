0ab038663340b8c63edebc3b3adeb8a1d5367914
geovanegottschalkavila-stack <geovanegottschalkavila@gmail.com>
2026-09-21 21:11:58 -0300

    for +2..

--- main.c
+++ main.c
@@ -7,11 +7,9 @@
        SetConsoleCP(65001);
        SetConsoleOutputCP(65001);
 
-       int idade[3] = {25, 35, 45};
-
-       printf("%s %d %s", "Manoel tem, ", idade[0], "anos!");
-       printf("%s %d %s", "\nlucia tem ", idade[1], "anos!");
-       printf("%s %d %s", "\nmartim tem ", idade[2], "anos!");
+       for(int i = 0; i <= 10; i = i +2){
+            printf("%d\n", i);
+       }
 
         return 0;
 
