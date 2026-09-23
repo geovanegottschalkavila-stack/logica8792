2e8c295ecec0fc9f349ac6952a17cfc29fae8159
geovanegottschalkavila-stack <geovanegottschalkavila@gmail.com>
2026-09-22 21:13:20 -0300

    for, decrecente54321

--- main.c
+++ main.c
@@ -1,36 +1,19 @@
 #include<stdio.h>
 #include<windows.h>
 
-    int somar(int a, int b){
-        return a + b;
-    }
-    int subtrair(int a, int b){
-        return a - b;
-    }
-    int multiplicar( int a, int b){
-        return a * b;
-    }
-    int dividir(int a, int b){
-        if(b  <= 0){
-            printf("Ele precisa ser maior que 0!");
-        }else{
-            return a / b;
-        }
-    }
+        
 
     int main(){
 
        SetConsoleCP(65001);
        SetConsoleOutputCP(65001);
 
-        int resultado1 = somar(5, 3);
-        int resultado2 = subtrair(5, 3);
-        int resultado3 = multiplicar(5, 3);
-        int resultado4 = dividir(16, 2);
-       printf("O resultado da soma é: %d\n", resultado1);
-       printf("O resultado da subtração é: %d\n", resultado2);
-       printf("o resultado da multiplicação é: %d\n", resultado3);
-       printf("O resultado da divisão é: %d\n", resultado4);
+        int num[5] = {1, 2, 3, 4, 5};
+
+        for(int i = 0; i < 5; i++){
+            printf("%d\n", num[i]);
+        }
+
 
 
         return 0;
