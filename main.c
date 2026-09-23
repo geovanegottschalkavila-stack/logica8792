99236eb4dec0d0bd3497efbc085ff28a879ee50d
geovanegottschalkavila-stack <geovanegottschalkavila@gmail.com>
2026-09-23 20:59:19 -0300

    verificarEhParImpar

--- main.c
+++ main.c
@@ -2,23 +2,21 @@
 #include<windows.h>
 #include<math.h>
 
-    int tabuada(int n){
-        printf("tabuada do %d:\n", n);
-        for(int i = 1; i <= 10; i++){
-            printf("%d x %d = %d\n", n, i, n *i);
-        } 
-        return 0;
+    void verificarEhParImpar(int n){
+        if(n % 2 == 0){
+            printf("%d é ímpar\n", n);
+        }else{
+            printf("%d é ímpar\n", n);
+        }
     }
 
     int main(){
 
        SetConsoleCP(65001);
        SetConsoleOutputCP(65001);
-        
-        int numero;
-        printf("Digite um número para ver a tabuada: ");
-        scanf("%d", &numero);
-        tabuada(numero);
+       
+       verificarEhParImpar(10);
+       verificarEhParImpar(7);
        
 
         return 0;
