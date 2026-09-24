67c8ea62cb621df4fe66042b7dc66e0432cff043
geovanegottschalkavila-stack <geovanegottschalkavila@gmail.com>
2026-09-23 21:49:13 -0300

    máximo e mínimo

--- main.c
+++ main.c
@@ -3,27 +3,22 @@
 #include<math.h>
 #include<string.h>
 
-    void contarVogais(char palavra[]){
-        int contador = 0;
-        for(int i = 0; i <strlen(palavra); i++){
-            char c = palavra[i];
-            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
-            || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
-                contador++;
-            }//stren calcula os caracteres de uma palavra.
-        }
-        printf("A Palavra '%s' tem %d vogais.\n", palavra, contador);
+    void maxMin(int v[], int tamanho){
+        int max = v[0], min = v[0];
+        for(int i = 1; i < tamanho; i++){
+            if(v[i] > max) max = v[i];
+            if(v[i] < min) min = v[i];
     }
+    printf("Máximo: %d, Mínimo: %d\n", max, min);
+}
+
     int main(){
 
        SetConsoleCP(65001);
        SetConsoleOutputCP(65001);
        
-       char p[20];
-       printf("Digite um a palavra para verificar as vogais: ");
-       scanf("%s", p);
-
-       contarVogais(p);
+       int numeros[] = {4, 8, 2, 15, 6};
+       maxMin(numeros, 5);
        
         return 0;
 
