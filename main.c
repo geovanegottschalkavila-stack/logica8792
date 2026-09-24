69e8d0862133b4c90d59e09aae6571243bbae3bd
geovanegottschalkavila-stack <geovanegottschalkavila@gmail.com>
2026-09-24 20:36:17 -0300

    for de 2 em dois

--- main.c
+++ main.c
@@ -10,12 +10,8 @@
        SetConsoleCP(65001);
        SetConsoleOutputCP(65001);
        
-       int i = 1;
-       while(i <= 50){
-            if(i % 2 == 0){
-                printf("%d\n", i);
-            }
-            i++;
+       for(int i = 0; i <= 50; i += 2){
+            printf("%d\n", i);
        }
 
         return 0;
