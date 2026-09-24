41782e9969507fba69b365668513395dd9bc5af6
geovanegottschalkavila-stack <geovanegottschalkavila@gmail.com>
2026-09-23 21:31:08 -0300

    contar vogais com digite ususario

--- main.c
+++ main.c
@@ -1,21 +1,30 @@
 #include<stdio.h>
 #include<windows.h>
 #include<math.h>
+#include<string.h>
 
-    void linha(int tamanho){
-        for(int i = 0; i < tamanho; i++){
-            printf("*");
+    void contarVogais(char palavra[]){
+        int contador = 0;
+        for(int i = 0; i <strlen(palavra); i++){
+            char c = palavra[i];
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+            || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
+                contador++;
+            }//stren calcula os caracteres de uma palavra.
         }
-        printf("\n");
+        printf("A Palavra '%s' tem %d vogais.\n", palavra, contador);
     }
-
     int main(){
 
        SetConsoleCP(65001);
        SetConsoleOutputCP(65001);
        
-       linha(2000);
+       char p[20];
+       printf("Digite um a palavra para verificar as vogais: ");
+       scanf("%s", p);
 
+       contarVogais(p);
+       
         return 0;
 
     }
