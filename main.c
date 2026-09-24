64f558e9c3136007e1af03fb9792ab4dab2b0d66
geovanegottschalkavila-stack <geovanegottschalkavila@gmail.com>
2026-09-24 19:47:27 -0300

    while+switchbanco

--- main.c
+++ main.c
@@ -10,15 +10,26 @@
        SetConsoleCP(65001);
        SetConsoleOutputCP(65001);
        
-       int numero;
-       printf("Digite um numero positivo:");
-       scanf("%d", &numero);
+       int opcao = -1;
 
-       while(numero <= 0){
-            printf("Numero inválido! Digite novamente: ");
-            scanf("%d", &numero);
+       while(opcao != 0){
+            printf("\nMenu: \n");
+            printf("1 - Depositar\n");
+            printf("2 - Sacar\n");
+            printf("3 - Consultar\n");
+            printf("0 - Saír\n");
+            printf("Escolha: ");
+            scanf("%d", &opcao);
+
+            switch(opcao){
+                case 1: printf("Você escolheu Depositar\n"); break;
+                case 2: printf("Você escolheu Sacar\n"); break;
+                case 3: printf("Você escolheu Consultar\n"); break;
+                case 0: printf("Saindo..."); break;
+                default: printf("Opção invválida\n");
+            }
        }
-       printf("Numero válido: %d\n", numero);
+
         return 0;
 
     }
