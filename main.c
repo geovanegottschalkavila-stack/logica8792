499ab1b76002ce359ec743fdfc0d4c4f2ce3427d
geovanegottschalkavila-stack <geovanegottschalkavila@gmail.com>
2026-09-24 21:53:18 -0300

    medias em for

--- main.c
+++ main.c
@@ -11,20 +11,17 @@
        SetConsoleOutputCP(65001);
        
        int n;
-       int resultado;
+       float valor, soma = 0;
 
-       printf("Digite um numero para a tabuada: ");
-       resultado = scanf("%d", &n);
+       printf("Quantos número deseja digitar: ");
+       scanf("%d", &n);
 
-       if(resultado != 1){
-            printf("Entrada invalia! por favor, digite apenas números!");
-            return 1;
+       for(int i = 0; i < n; i++){
+            printf("Digite o numero %d: ", i + 1);
+            scanf("%f", &valor);
+            soma += valor;
        }
-
-       for(int i = 1; i <= 10; i++){
-            int resposta = n * i;
-            printf("O resultado de %d x %d é: %d\n", n, i, resposta);
-       }
+       printf("Média: %.2f\n", soma / n);
            
 
         return 0;
