ac18d9d952ea432156bc42c3b0cd0d86f72e2b92
geovanegottschalkavila-stack <geovanegottschalkavila@gmail.com>
2026-09-25 19:48:07 -0300

    numeros primos com função

--- main.c
+++ main.c
@@ -3,31 +3,26 @@
 #include<math.h>
 #include<string.h>
 
-    
+    int ehPrimo(int n){
+        if(n < 2)return 0;
+        for(int i = 2; i < n; i++){
+            if(n % i == 0)return 0;
+        }
+        return 1;
+    }
 
     int main(){
 
        SetConsoleCP(65001);
        SetConsoleOutputCP(65001);
        
-       int n, primo = 1;
+       int numero;
        printf("Digite um numero: ");
-       scanf("%d", &n);
-       if(n < 2){
-            primo = 0;
-       }else{
-        for(int i = 2; i <= n / 2; i++){
-            if(n % i == 0){
-                primo = 0;
-                break;
-            }
-        }
-       }
-       if(primo){
-            printf("%d é primo\n", n);
-       }else{
-            printf("%d não é primo\n", n);
-       }
+       scanf("%d", &numero);
+       if(ehPrimo(numero))
+            printf("%d é primo\n", numero);
+        else
+            printf("%d não é primo\n", numero);
 
         return 0;
 
