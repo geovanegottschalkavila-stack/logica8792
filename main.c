57c7f4d4e31fdbcc0eac29037f86c511cc911309
geovanegottschalkavila-stack <geovanegottschalkavila@gmail.com>
2026-09-25 20:09:15 -0300

    num. perfeito

--- main.c
+++ main.c
@@ -3,26 +3,26 @@
 #include<math.h>
 #include<string.h>
 
-    int ehPrimo(int n){
-        if(n < 2)return 0;
-        for(int i = 2; i < n; i++){
-            if(n % i == 0)return 0;
-        }
-        return 1;
-    }
 
     int main(){
 
        SetConsoleCP(65001);
        SetConsoleOutputCP(65001);
        
-       int numero;
+       int n, soma = 0;
        printf("Digite um numero: ");
-       scanf("%d", &numero);
-       if(ehPrimo(numero))
-            printf("%d é primo\n", numero);
-        else
-            printf("%d não é primo\n", numero);
+       scanf("%d", &n);
+       
+       for(int i = 1; i < n; i++){
+            if(n % i == 0){
+                soma += i;
+            }
+       }
+       if(soma == n){
+            printf("%d é um numero perfeiro\n", n);
+       }else{
+            printf("%d Não é um número perfeito\n", n);
+       }
 
         return 0;
 
