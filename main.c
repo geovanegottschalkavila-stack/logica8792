8e2c8feac89b9e78bc89632f53e625a683efd46d
geovanegottschalkavila-stack <geovanegottschalkavila@gmail.com>
2026-09-25 19:08:28 -0300

    contador de números while

--- main.c
+++ main.c
@@ -10,19 +10,16 @@
        SetConsoleCP(65001);
        SetConsoleOutputCP(65001);
        
-       int n;
-       float valor, soma = 0;
+       int numero, contador = 0;
 
-       printf("Quantos número deseja digitar: ");
-       scanf("%d", &n);
+       printf("Digite números(0 para parar): \n");
+       scanf("%d", &numero);
 
-       for(int i = 0; i < n; i++){
-            printf("Digite o numero %d: ", i + 1);
-            scanf("%f", &valor);
-            soma += valor;
+       while(numero != 0){
+            contador++;
+            scanf("%d", &numero);
        }
-       printf("Média: %.2f\n", soma / n);
-           
+       printf("Quantidade de numeros digitados: %d\n", contador);
 
         return 0;
 
