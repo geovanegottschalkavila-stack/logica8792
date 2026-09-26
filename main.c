7983fcf00bbf107638e69852b792625fb93201a1
geovanegottschalkavila-stack <geovanegottschalkavila@gmail.com>
2026-09-25 21:49:59 -0300

    operações matemáticas

--- main.c
+++ main.c
@@ -9,18 +9,48 @@
        SetConsoleCP(65001);
        SetConsoleOutputCP(65001);
        
-       int n;
-       float nota, soma = 0;
-       
-       printf("Quantos alunoes tem a turma: ");
-       scanf("%d", &n);
-       for(int i = 0; i < n; i++){
-        printf("Digite a nota do aluno %d: \n", i + 1);
-        scanf("%f", &nota);
-        soma += nota;
-       }
+       int opcao;
+       float a, b;
+       do{
+        printf("\n----Menú----\n");
+        printf("1 - soma\n");
+        printf("2 - Subtração\n");
+        printf("3 - Multiplicação\n");
+        printf("4 - Divisão\n");
+        printf("0 - Saír\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
 
-       printf("Média da turma: %.2f\n", soma / n);
+        switch(opcao){
+            case 1:
+                printf("Digite dois números: ");
+                scanf("%f %f", &a, &b);
+                printf("Resultado: %.2f\n", a + b);
+                break;
+            case 2:
+                printf("Digite dois números: ");
+                scanf("%f %f", &a, &b);
+                printf("Resultado: %.2f\n", a - b);
+                break;
+            case 3:
+                printf("Digite um numero: ");
+                scanf("%F %F", &a, &b);
+                printf("Resultado: %.2f\n", a * b);
+                break;
+            case 4:
+                printf("Digite um número: ");
+                scanf("%F %f", &a, &b);
+                printf("Resultado: %.2f\n", a / b);
+                break;
+            case 0:
+                printf("Saindo.....");
+                break;
+            default:
+                printf("Opção inválida!\n");
+                break;
+                
+        }
+       }while(opcao != 0);
 
         return 0;
 
